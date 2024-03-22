3b2d2481cefdee150e3689afd39a008defbc4e4c
zaidanaljauhar <zaidan_23520002@stimata.ac.id>
2024-03-22 19:21:50 +0700

    update

---
+++ DATABASE/properti.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 15, 2024 at 07:18 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `katalogpropertirumah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `properti`
+--
+
+CREATE TABLE `properti` (
+  `id_PropertiRumah` int NOT NULL,
+  `alamat` int NOT NULL,
+  `tipe_rumah` int NOT NULL,
+  `luas_tanah` int NOT NULL,
+  `luas_bangunan` int NOT NULL,
+  `harga` int NOT NULL,
+  `jumlah_kamar` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
